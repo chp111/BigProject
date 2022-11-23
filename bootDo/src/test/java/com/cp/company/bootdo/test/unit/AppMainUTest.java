3281244e25863a656b1265fd40b88279b0736990
陈朋 <chp_111@163.com>
2022-11-23 12:55:30 +0800

    初始化一个spring boot web基础工程

---
+++ bootDo/src/test/java/com/cp/company/bootdo/test/unit/AppMainUTest.java
@@ -0,0 +1,14 @@
+package com.cp.company.bootdo.test.unit;
+
+
+import org.junit.Test;
+
+public class AppMainUTest {
+
+    @Test
+    public void test01() {
+        System.out.println("启动类--单元测试");
+    }
+
+
+}
