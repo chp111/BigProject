cb60a45aa3ad2b358ccbe4db16253efd01dbf370
陈朋 <chp_111@163.com>
2022-11-25 14:19:14 +0800

    微服务集成eureka单机版

--- MS-Hoxton-SR1/cloud-consumer-order80/src/main/java/com/cp/company/OrderMain80.java
+++ MS-Hoxton-SR1/cloud-consumer-order80/src/main/java/com/cp/company/OrderMain80.java
@@ -3,6 +3,7 @@ package com.cp.company;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author 陈朋
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.cp.company.mapper")
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
