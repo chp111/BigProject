d9dd78fc961dd4645ea9e5a1cf54a7961d968f38
陈朋 <chp_111@163.com>
2022-11-26 22:00:29 +0800

    微服务-基于Eureka服务注册中心, 开发Ribbon 负载均衡案例-1

--- MS-Hoxton-SR1/cloud-consumer-order80/src/main/java/com/cp/company/OrderMain80.java
+++ MS-Hoxton-SR1/cloud-consumer-order80/src/main/java/com/cp/company/OrderMain80.java
@@ -1,9 +1,11 @@
 package com.cp.company;
 
+import com.cp.customrule.MySelfRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @author 陈朋
@@ -13,6 +15,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @MapperScan("com.cp.company.mapper")
 @EnableEurekaClient
+// 重新指定Ribbon负载均衡策略
+@RibbonClient(name = "CLOULD-PAYMENT-SERVICE", configuration = {MySelfRule.class})
 public class OrderMain80 {
 
     public static void main(String[] args) {
--- MS-Hoxton-SR1/cloud-consumer-order80/src/main/java/com/cp/company/controller/OrderController.java
+++ MS-Hoxton-SR1/cloud-consumer-order80/src/main/java/com/cp/company/controller/OrderController.java
@@ -2,7 +2,9 @@ package com.cp.company.controller;
 
 import com.cp.company.controller.vo.PaymentReqVO;
 import com.cp.company.utils.CommResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,7 @@ import java.util.UUID;
  */
 @RestController
 @RequestMapping("/order")
+@Slf4j
 public class OrderController {
 
     public static final String URL = "http://CLOULD-PAYMENT-SERVICE";
@@ -50,5 +53,21 @@ public class OrderController {
         return restTemplate.postForObject(URL + "/payment/queryDetail", reqVO, CommResult.class);
     }
 
+    /**
+     * 调用查询支付信息服务
+     *
+     * @param reqVO
+     * @return
+     */
+    @PostMapping("/queryDetailV2")
+    public CommResult queryDetailV2(@RequestBody PaymentReqVO reqVO) {
+        ResponseEntity<CommResult> entity =
+                restTemplate.postForEntity(URL + "/payment/queryDetail", reqVO, CommResult.class);
+        log.info("返回结果:{}", entity);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        }
+        return new CommResult(444, "调用失败");
+    }
 }
 
