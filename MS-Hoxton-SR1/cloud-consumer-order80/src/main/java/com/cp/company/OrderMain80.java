8d58f815adef6bb9dcb9cba29e0262bdf1ff7813
陈朋 <chp_111@163.com>
2022-11-26 20:53:54 +0800

    微服务Hoxton基于Consul注册中心开发案例

--- MS-Hoxton-SR1/cloud-consumer-order80/src/main/java/com/cp/company/OrderMain80.java
+++ MS-Hoxton-SR1/cloud-consumer-order80/src/main/java/com/cp/company/OrderMain80.java
@@ -3,7 +3,7 @@ package com.cp.company;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 陈朋
@@ -12,7 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @MapperScan("com.cp.company.mapper")
-@EnableEurekaClient
+@EnableDiscoveryClient //开启服务注册与发现
 public class OrderMain80 {
 
     public static void main(String[] args) {
